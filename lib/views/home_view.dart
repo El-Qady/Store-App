58c672831d0520dc03d042a7fa837360d3d9bbba
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-08-30 11:30:46 +0300

    add search field

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/widgets/search_field.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Color(0xff1c1c1c),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          SearchField(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/search_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 4.0), // Add padding to the TextField(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                contentPadding: EdgeInsets.all(0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide.none,
+                ),
+                prefixIcon: Icon(Icons.search, color: Colors.white54),
+                fillColor: Color(0xff383838),
+                labelText: 'Search',
+                labelStyle: TextStyle(color: Colors.white54),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 8.0),
+        Padding(
+          padding: EdgeInsets.only(right: 16.0),
+          child: Icon(Icons.shopping_cart, color: Colors.white54, size: 28),
+        ),
+      ],
+    );
+  }
+}
