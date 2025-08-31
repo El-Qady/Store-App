2751f66745a77d0e859b04d5d604a84577d30689
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-08-31 14:01:25 +0300

    add grid view builder

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,19 +10,32 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff1c1c1c),
+    return Scaffold(
+      backgroundColor: const Color(0xff1c1c1c),
       body: Column(
         children: [
-          SizedBox(height: 50),
-          SearchField(),
-          SizedBox(height: 20),
-          ProductItem(),
+          const SizedBox(height: 70),
+          const SearchField(),
+          const SizedBox(height: 10),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) {
+                return const ProductItem();
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
