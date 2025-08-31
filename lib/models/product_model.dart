02dc0fc7de9d25013eb2f356c10f2e2f533ede5e
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-08-31 21:46:19 +0300

    add dio package and add product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,47 @@
+class ProductModel {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String image;
+  final String category;
+  final Rating rating;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.category,
+    required this.rating,
+  });
+  factory ProductModel.fromJson(json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      price: json['price'],
+      image: json['image'],
+      category: json['category'],
+      rating: Rating.fromJson(json['rating']),
+    );
+  }
+}
+
+class Rating {
+  final double rate;
+  final int count;
+
+  Rating({
+    required this.rate,
+    required this.count,
+  });
+
+  factory Rating.fromJson(Map<String, dynamic> json) {
+    return Rating(
+      rate: json['rate'],
+      count: json['count'],
+    );
+  }
+}
