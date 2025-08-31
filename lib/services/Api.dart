f24dcd250279d425e8fa691d73d1029d1ad6a7a3
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-09-01 01:13:56 +0300

    add categories and product for categories

--- lib/services/Api.dart
+++ lib/services/Api.dart
@@ -2,9 +2,9 @@ import 'package:dio/dio.dart';
 
 class Api {
   final dio = Dio();
-  Future<dynamic> getapi() async {
+  Future<dynamic> getapi({required String url}) async {
     final response =
-        await dio.get('https://fakestoreapi.com/products');
+        await dio.get(url);
     if (response.statusCode == 200) {
       return response.data;
     } else {
---
+++ lib/services/get_category_service.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:store_app/models/product_model.dart';
+import 'package:store_app/services/get_all_product_service.dart';
+
+class GetCategoryService {
+  List<ProductModel> getCategoryProducts(String category) {
+    List<ProductModel> allProducts = AllProductService().getAllProducts();
+    List<ProductModel> categoryProducts = allProducts
+        .where((product) => product.category == category)
+        .toList();
+    return categoryProducts;
+  }
+}
