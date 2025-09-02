1085ca5ce23a8a44d807fe1d676540a3f22d1b8a
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-09-02 18:04:11 +0300

    create ui of update page

--- lib/views/update_product_view.dart
+++ lib/views/update_product_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/models/product_model.dart';
+import 'package:store_app/widgets/custom_input_field.dart';
 import 'package:store_app/widgets/product_item.dart';
 
 class UpdateProductView extends StatefulWidget {
@@ -11,6 +13,8 @@ class UpdateProductView extends StatefulWidget {
 class _UpdateProductViewState extends State<UpdateProductView> {
   @override
   Widget build(BuildContext context) {
+    final ProductModel product =
+        ModalRoute.of(context)!.settings.arguments as ProductModel;
     return Scaffold(
       backgroundColor: const Color(0xff1c1c1c),
       appBar: AppBar(
@@ -22,8 +26,60 @@ class _UpdateProductViewState extends State<UpdateProductView> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: const Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ProductItem(
+              onTap: () {},
+              product: product,
+            ),
+            const SizedBox(height: 20),
+            CustomInputField(
+              labelText: 'Title',
+              onSubmitted: (value) {},
+            ),
+            const SizedBox(height: 10),
+            CustomInputField(
+              labelText: 'Price',
+              onSubmitted: (value) {},
+            ),
+            const SizedBox(height: 10),
+            CustomInputField(
+              labelText: 'Description',
+              onSubmitted: (value) {},
+            ),
+            const SizedBox(height: 10),
+            CustomInputField(
+              labelText: 'Category',
+              onSubmitted: (value) {},
+            ),
+            const SizedBox(height: 10),
+            CustomInputField(
+              labelText: 'Image URL',
+              onSubmitted: (value) {},
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Update',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
